1e92c23653ea18839c2b4c68c42d9da543395f16
Ben-JClark <benclarkat@proton.me>
2023-12-27 23:50:09 +1300

    updated navbar UI

--- client/minecraftbuilds/src/App.tsx
+++ client/minecraftbuilds/src/App.tsx
@@ -7,15 +7,16 @@ function App() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
+        <Navbar.Brand href="#servers">Servers |</Navbar.Brand>
         <Navbar.Brand href="#home">HubCraft</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#Bases">Bases</Nav.Link>
-            <Nav.Link href="#Shops">Shops</Nav.Link>
-            <Nav.Link href="#Farms">Farms</Nav.Link>
-            <Nav.Link href="#People">People</Nav.Link>
+            <Nav.Link href="#bases">Bases</Nav.Link>
+            <Nav.Link href="#shops">Shops</Nav.Link>
+            <Nav.Link href="#farms">Farms</Nav.Link>
+            <Nav.Link href="#players">Players</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
