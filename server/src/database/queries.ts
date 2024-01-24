bd85bc65bdf08942e8e306303d3d872b66db52fd
Ben-JClark <benclarkat@proton.me>
2024-01-24 16:31:28 +1300

    Added server route for player bases
    server.ts has a new route that will call a function from queries.ts to obtain a list of player bases for that server

--- server/src/database/queries.ts
+++ server/src/database/queries.ts
@@ -50,4 +50,29 @@ async function getLongDescription(id: number) {
   }
 }
 
-export { getServers, getLongDescription };
+/**
+ * Query the mysql database to get a bases for a particular server
+ * @returns null or a list of bases in json format
+ */
+async function getBases(serverID: number) {
+  let connection;
+  try {
+    connection = await pool.getConnection();
+    const [response] = await connection.query("CALL get_bases(?)", [serverID]);
+    if (Array.isArray(response)) {
+      return response[0];
+    } else {
+      console.log("Error: Response from sql was not in array format");
+      return null;
+    }
+  } catch (error) {
+    console.log("Error getting the list of bases: ", error);
+    return null;
+  } finally {
+    if (connection !== undefined) {
+      connection.release();
+    }
+  }
+}
+
+export { getServers, getLongDescription, getBases };
--- server/src/server.ts
+++ server/src/server.ts
@@ -2,7 +2,7 @@ import express, { Express, Request, Response } from "express";
 import cors from "cors";
 import morgan from "morgan";
 const app: Express = express();
-import { getServers, getLongDescription } from "./database/queries.js";
+import { getServers, getLongDescription, getBases } from "./database/queries.js";
 
 app.use(express.urlencoded({ extended: true }));
 
@@ -15,7 +15,7 @@ app.get("/", async (req: Request, res: Response) => {
   const serverList = await getServers();
   if (serverList !== null) {
     console.log("Sending server list", serverList);
-    res.status(200).json(serverList); // Send it as JSON response
+    res.status(200).json(serverList);
   } else {
     console.log("Error sending server list");
     res.status(500).json({ error: "Failed to retreive server list." });
@@ -25,10 +25,20 @@ app.get("/", async (req: Request, res: Response) => {
 app.get("/server/:serverName/:serverID/home", async (req: Request, res: Response) => {
   const description = await getLongDescription(parseInt(req.params.serverID));
   if (description !== null) {
-    res.status(200).json(description); // getLongDescription has already fromatted as JSON response
+    res.status(200).json(description);
   } else {
     console.log("Error sending server data");
     res.status(500).json({ error: "Failed to retreive server data." });
+  }
+});
+
+app.get("/server/:serverName/:serverID/bases", async (req: Request, res: Response) => {
+  const baseList = await getBases(parseInt(req.params.serverID));
+  if (baseList !== null) {
+    res.status(200).json(baseList);
+  } else {
+    console.log("Error sending the list of bases");
+    res.status(500).json({ error: "Failed to retreive the list of bases." });
   }
 });
 
